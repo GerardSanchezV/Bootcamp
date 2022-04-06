4c759477b96f019ee034143fe4d79991d4814e14
Sanchez <Sanchez@DESKTOP-B5MPBG8>
2022-04-06 13:04:46 +0200

    Bootcamp

---
+++ Ejemplo/src/EX4/Ex2App.java
@@ -0,0 +1,29 @@
+package EX4;
+
+public class Ex2App {
+
+	public static void main(String[] args) {
+		int N=5;
+		double A=7.75;
+		char C='a';
+		double suma=0;
+		double resta=0;
+		
+		suma=N+A;
+		resta=A-N;
+		
+		
+		
+		System.out.println("Variable N = "+5);
+		System.out.println("Variable A = "+7.75);
+		System.out.println("Variable C = "+'a');
+		System.out.println(+N+" + "+A+"="+suma);
+		System.out.println(+A+" - "+N+"="+resta);
+		System.out.println("Valor numérico del carácter'a'");
+		}
+		
+}		
+
+	
+
+
