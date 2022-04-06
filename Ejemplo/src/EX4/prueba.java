73da0ae45dcd7dc59cb5afa9b6d4e475fd88143d
Sanchez <Sanchez@DESKTOP-B5MPBG8>
2022-04-06 14:26:18 +0200

    ex3

---
+++ Ejemplo/src/EX4/prueba.java
@@ -0,0 +1,47 @@
+package EX4;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		int x=4;
+		int y=8;
+		
+		double n=50;
+		double m=20;
+		
+		System.out.println("Variable x = "+4);
+		System.out.println("Variable y = "+8);
+		System.out.println("Variable n = "+50);
+		System.out.println("Variable m = "+20);
+		
+		System.out.println("La suma de X+Y es = "+(x+y));
+		System.out.println("La resta de X-Y es = "+(x-y));
+		System.out.println("El producto de X*Y es = "+(x*y));
+		System.out.println("El cociente de X/Y es = "+(x/y));
+		System.out.println("El resto de X%Y es = "+(x%y));
+		
+		System.out.println("El suma es = "+(n+m));
+		System.out.println("La resta es = "+(n-m));
+		System.out.println("El producto es = "+(n*m)) ;
+		System.out.println("El cociente es = "+(n/m));
+		System.out.println("El resto es = "+(n%m));
+		
+		System.out.println("La suma es = "+(x+n));
+		System.out.println("El cociente es = "+(y/m));
+		System.out.println("El resto es = "+(y%m));
+		
+		System.out.println("El doble es = "+(2*x));
+		System.out.println("El doble es = "+(2*y));
+		System.out.println("El doble es = "+(2*n));
+		System.out.println("El doble es = "+(2*m));
+	
+		System.out.println("La suma de todas las variables es = "+(x+y+n+m));
+		System.out.println("El producto de todas las variables es = "+(x*y*n*m));
+		
+		
+		
+		
+
+	}
+
+}
